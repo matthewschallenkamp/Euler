f4d5d781b9ab9692624c055b0e0b98f6aead07d9
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2015-10-20 16:05:23 -0600

    did #7

---
+++ 7.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+//100001th prime
+
+int nth_prime(int n)
+{
+    static set<int> primes = {2, 3, 5, 7, 11, 13};
+    if(n > (primes.size()))
+    {
+        for(int i = *(primes.rbegin()); primes.size() < n+10; i += 2)
+        {
+            bool prime = true;
+            for(auto it : primes)
+            {
+                if(i % it == 0)
+                {
+                    prime = false;
+                    break;
+                }
+                if(it*it > i + 10)
+                    break;
+            }
+            if(prime)
+                primes.insert(i);
+        }
+    }
+    set<int>::iterator it = primes.begin();
+    for(int i = 1; i < n; i++)
+        it++;
+    return *it;
+
+}
+
+int main()
+{
+    cout << "the 10001st prime is " << nth_prime(10001);
+    return 0;
+}
