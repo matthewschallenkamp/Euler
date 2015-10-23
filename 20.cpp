0b1773a27c7d7f76faa6903f246ef2cff86061e1
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2015-10-22 20:30:23 -0600

    did #20

---
+++ 20.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+
+vector<int> digits(int n)
+{
+    vector<int> m;
+    while(n > 0)
+    {
+        m.push_back(n % 10);
+        n /= 10;
+    }
+    return m;
+}
+
+int main()
+{
+        //sum digits 2^1000
+    vector<int> v = {1};
+    for(int i = 1; i < 100; i++)
+    {
+        for(auto &item : v)
+            item *= i;
+        for(int i = 1; i < v.size(); i++)
+        {
+            while(v[i-1] >= 10)
+            {
+                v[i-1] -= 10;
+                v[i]++;
+            }
+        }
+        while(v.back() >= 10)
+        {
+            int t = v.back() / 10;
+            v.back() %= 10;
+            v.push_back(t);
+        }
+
+        for(auto i : v)
+            cout << i;
+        cout << endl;
+
+    }
+    int t = 0;
+    for(auto i : v)
+        t += i;
+    cout << "the sum of the digits was " << t;
+    return 0;
+}
