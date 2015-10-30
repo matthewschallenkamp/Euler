86fad8381420ab1557b365bfb053e011d205a3b8
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2015-10-30 09:44:15 -0600

    did #24

---
+++ 24.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+vector<int> nth_permute_s(int &n, int s)
+{
+    vector<int> v;
+    for(int i = 0; i < s; i++)
+    {
+        v = nth_permute_s(n, s-1);
+        if(n == 0)
+        {
+            for(int &num : v)
+                if(num >= i)
+                    num++;
+            v.push_back(i);
+            return v;
+
+        }
+    }
+    if(s == 0)
+        n--;
+    return {};
+}
+
+int main()
+{
+    int nth = 1000000;
+    vector<int> v = nth_permute_s(nth, 10);
+    for(int i = v.size()-1; i >= 0; i--)
+        cout << v.at(i);
+
+    return 0;
+}
