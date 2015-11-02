87a0fe483441b4fb2b945355fa2fa9d10b63af23
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2015-11-02 14:50:10 -0700

    did #48

---
+++ 48.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+
+int main()
+{
+    long long digits = 0;
+    for(int i = 1; i < 1000; i++)
+    {
+        long long mult = 1;
+        for(int pow = 0; pow < i; pow++)
+        {
+            mult *= i;
+            mult %= 10000000000;
+        }
+        digits += mult;
+        digits %= 10000000000;
+    }
+
+    cout << "the last 10 digits are " << digits;
+
+    return 0;
+}
