63817f0a7bb4438b403bf3ffd210021779a06c1f
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2015-11-02 09:14:03 -0700

    did #42 with modified words.txt

---
+++ 42.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+bool is_triangle(long long n)
+{
+    n *= 2;
+    long long sq = sqrt(n);
+    for(long long i = sq-5; i < sq+5; i++)
+    {
+        if(i * (i+1) == n)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    ifstream fin;
+    fin.open("p042_words.txt");
+    if(!fin)
+        return 0;
+    int c = 0;
+    long long n;
+    string s;
+    while(fin >> s)
+    {
+        n = 0;
+        for(char c : s)
+            n += c-'A' + 1;
+        if(is_triangle(n))
+        {
+            cout << s << " gives " << n << " which is " << "a triangle" << endl;
+            c++;
+        }
+        else
+            cout << s << " gives " << n << " which is " << "not " << "a triangle" << endl;
+
+    }
+    cout << "the final count was " << c << endl;
+
+    return 0;
+}
