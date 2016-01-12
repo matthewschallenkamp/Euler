03930971a05dfb74109d2aa64fdc728ca1c0e61c
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2016-01-12 13:46:22 -0700

    did 53

---
+++ 53.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+double ncr(long long n, long long r)
+{
+    double total = 1;
+    long long oldn = n;
+    while(n > r)
+        total *= n--;
+    r = oldn - r;
+    while(r > 1)
+        total /= r--;
+    return total;
+}
+
+int main()
+{
+    int sum = 0;
+
+    for(int n = 1; n <= 100; n++)
+    {
+       for(int r = 0; r <= n; r++)
+       {
+            if(ncr(n,r) > 1000000)
+            {
+                sum += 1;
+                cout << n << " c " << r << " = " << ncr(n,r) << endl;
+            }
+       }
+    }
+
+    cout << "the total over 1 mill was " << sum;
+    return 0;
+}
