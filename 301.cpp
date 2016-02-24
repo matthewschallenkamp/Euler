d6160603e1999e1d5a8a9e59e8a06a412d18da95
matthewschallenkamp <matthew.schallenkamp@gmail.com>
2016-02-23 23:13:43 -0700

    did 301

---
+++ 301.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include "BigInt.h"
+
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+
+int main()
+{
+    ull total = 0;
+    for(long long n = 1; n <= 1073741824; n++)
+    {
+        if(!(n % 1000000))
+            cout << n << endl;
+        total += ((n xor 2*n xor 3*n) == 0);
+    }
+    cout << "the total was " << total;
+    return 0;
+}
+
